866f74c33f87c93dd24f1850eb61f1505da1ec97
kazurayam <kazuaki.urayama@gmail.com>
2025-08-02 08:15:07 +0900

    save progress

--- app/src/main/groovy/com/kazurayam/groovy/CompilePhasesDiffer.groovy
+++ app/src/main/groovy/com/kazurayam/groovy/CompilePhasesDiffer.groovy
@@ -11,54 +11,78 @@ import org.codehaus.groovy.control.CompilePhase
 import java.nio.file.Files
 import java.nio.file.Path
 
+/**
+ *
+ */
 class CompilePhasesDiffer {
 
-    static Path report(String identifier, String sourceCode, Path outDir) {
+    /**
+     *
+     * @param groovySource
+     * @param identifier
+     * @param outDir
+     * @return
+     */
+    static Path report(String groovySource, String identifier, Path outDir) {
+        // the params should not be null
+        Objects.requireNonNull(groovySource)
         Objects.requireNonNull(identifier)
-        Objects.requireNonNull(sourceCode)
         Objects.requireNonNull(outDir)
 
         // replace non-file-path-comprising-characters in the identifier
-        String escapedName = escape(identifier)
+        String escapedId = escape(identifier)
 
+        // ensure the output directory
         Files.createDirectories(outDir)
 
+        // instantiate the CompilationUnit object
         CompilationUnit cu = new CompilationUnit()
-        cu.addSource(escapedName, sourceCode)
+        cu.addSource(escapedId, groovySource)
         cu.compile()
 
+        // AstNodeToScriptAdapter enables us to unparse the Abstract Syntax Tree
         AstNodeToScriptAdapter adapter = new AstNodeToScriptAdapter()
 
+        // iterate over all entries of CompilePhases
         Map<CompilePhase, UnparseEntity> phases = new HashMap<>()
         for (CompilePhase phase : CompilePhase.values()) {
-            String rebuiltSource = adapter.compileToScript(sourceCode, phase.getPhaseNumber())
-            UnparseEntity ue = new UnparseEntity(identifier: escapedName, source: rebuiltSource)
+            // unparse the AST
+            String rebuiltSource = adapter.compileToScript(groovySource, phase.getPhaseNumber())
+            // memorize the rebuilt Groovy source
+            UnparseEntity ue = new UnparseEntity(identifier: escapedId, source: rebuiltSource)
             phases.put(phase, ue)
+            // save the rebuilt Groovy source of each CompilePhase into a file in the output directory
             Path outFile = outDir.resolve(
-                    "${escapedName}-${phase.getPhaseNumber()}_${phase.toString()}.groovy")
+                    "${escapedId}-${phase.getPhaseNumber()}_${phase.toString()}.groovy")
             outFile.text = rebuiltSource
         }
 
+        // generate the report
         StringBuilder sb = new StringBuilder()
         sb.append("# How Groovy compiler transforms a source\n\n")
-
         sb.append("Groovy Compiler transforms the Abstract Syntax Tree of \"${identifier}\" gradually." +
                 " This report shows how the AST Transformation progresses by looking at the diffs each CompilePhase.\n\n")
-
-        reportSection(sb, phases, CompilePhase.INITIALIZATION, CompilePhase.PARSING)
-        reportSection(sb, phases, CompilePhase.PARSING, CompilePhase.CONVERSION)
-        reportSection(sb, phases, CompilePhase.CONVERSION, CompilePhase.SEMANTIC_ANALYSIS)
-        reportSection(sb, phases, CompilePhase.SEMANTIC_ANALYSIS, CompilePhase.CANONICALIZATION)
-        reportSection(sb, phases, CompilePhase.CANONICALIZATION, CompilePhase.INSTRUCTION_SELECTION)
-        reportSection(sb, phases, CompilePhase.INSTRUCTION_SELECTION, CompilePhase.CLASS_GENERATION)
-        reportSection(sb, phases, CompilePhase.CLASS_GENERATION, CompilePhase.OUTPUT)
-        reportSection(sb, phases, CompilePhase.OUTPUT, CompilePhase.FINALIZATION)
-
-        Path report = outDir.resolve("${escapedName}-CompilePhasesDiff.md")
+        // generate sections that present the diff of ASTs
+        reportSubsection(sb, phases, CompilePhase.INITIALIZATION, CompilePhase.PARSING)
+        reportSubsection(sb, phases, CompilePhase.PARSING, CompilePhase.CONVERSION)
+        reportSubsection(sb, phases, CompilePhase.CONVERSION, CompilePhase.SEMANTIC_ANALYSIS)
+        reportSubsection(sb, phases, CompilePhase.SEMANTIC_ANALYSIS, CompilePhase.CANONICALIZATION)
+        reportSubsection(sb, phases, CompilePhase.CANONICALIZATION, CompilePhase.INSTRUCTION_SELECTION)
+        reportSubsection(sb, phases, CompilePhase.INSTRUCTION_SELECTION, CompilePhase.CLASS_GENERATION)
+        reportSubsection(sb, phases, CompilePhase.CLASS_GENERATION, CompilePhase.OUTPUT)
+        reportSubsection(sb, phases, CompilePhase.OUTPUT, CompilePhase.FINALIZATION)
+        // write the report
+        Path report = outDir.resolve("${escapedId}-CompilePhasesDiff.md")
         report.text = sb.toString()
         return report
     }
 
+    /**
+     * Escape characters in the str that should not be used in a file path string
+     * into safe characters
+     * @param str
+     * @return escaped string
+     */
     private static String escape(String str) {
         return str
                 .replaceAll("\\\\", '_')
@@ -73,9 +97,9 @@ class CompilePhasesDiffer {
     }
 
     /**
-     *
+     * compile a Markdown subsection
      */
-    private static void reportSection(StringBuilder sb,
+    private static void reportSubsection(StringBuilder sb,
                                       Map<CompilePhase, UnparseEntity> phases,
                                       CompilePhase leftPhase,
                                       CompilePhase rightPhase) {
